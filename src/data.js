18f7f51cf55f1315b9f230ce0785bd1623b23713
priyapandey28 <121967472+priyapandey28@users.noreply.github.com>
2023-01-10 21:29:11 +0530

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,78 @@
+// data that the student should use in his assigment
+
+let data = [
+    {
+        title: "Recipe1",
+        description: "description1",
+        category: "category1",
+        ingredients: "ingredients1"
+    },
+    {
+        title: "Recipe2",
+        description: "description2",
+        category: "category2",
+        ingredients: "ingredients2"
+    },
+    {
+        title: "Recipe3",
+        description: "description3",
+        category: "category3",
+        ingredients: "ingredients3"
+    },
+    {
+        title: "Recipe4",
+        description: "description4",
+        category: "category4",
+        ingredients: "ingredients4"
+    },
+    {
+        title: "Recipe5",
+        description: "description5",
+        category: "category5",
+        ingredients: "ingredients5"
+    },
+    {
+        title: "Recipe6",
+        description: "description6",
+        category: "category6",
+        ingredients: "ingredients6"
+    },
+    {
+        title: "Recipe7",
+        description: "description7",
+        category: "category7",
+        ingredients: "ingredients7"
+    },
+    {
+        title: "Recipe8",
+        description: "description8",
+        category: "category8",
+        ingredients: "ingredients8"
+    },
+    {
+        title: "Recipe9",
+        description: "description9",
+        category: "category9",
+        ingredients: "ingredients9"
+    },
+    {
+        title: "Recipe10",
+        description: "description10",
+        category: "category10",
+        ingredients: "ingredients10"
+    },
+    {
+        title: "Recipe11",
+        description: "description11",
+        category: "category11",
+        ingredients: "ingredients11"
+    },
+    {
+        title: "Recipe12",
+        description: "description12",
+        category: "category12",
+        ingredients: "ingredients12"
+    },
+];
+
+module.exports = data;
